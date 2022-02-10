e10e15c3324f9a2b77a7886b8e643fa01ca4b5be
codingmohamed <cloudchronicles92@gmail.com>
2022-02-10 15:41:15 +0000

    first commit

---
+++ words.py
@@ -0,0 +1,19 @@
+word_list = [
+'reet',
+'two',
+'double',
+'deep',
+'healthy',
+'force',
+'glorious',
+'boorish',
+'hallowed',
+'substance',
+'gleaming',
+'trot',
+'jumpy',
+'arrange',
+'wrist',
+'lackadaisical',
+]
+
